44e845e3904cf389ba88c312e55e42e39e325971
Sharat M R <cosmosgenius@gmail.com>
2014-07-04 22:37:42 +0530

    added code  for string permutation

---
+++ stringpermutations.py
@@ -0,0 +1,29 @@
+print("Enter the string you want to find permutation for")
+inputstr = input(">");
+def findPermutation(str):
+    return permute(list(str))
+
+def permute(list):
+    if len(list) == 1 :
+        return list
+
+    retlist = []
+
+    for x in list:
+        listcopy = list[:]
+        listcopy.remove(x)
+        for y in permute(listcopy):
+            retlist.append(x+y)
+    return retlist
+
+        
+def tostr(list):
+    retstr = ''
+    for x in list:
+        retstr += x
+    return retstr
+
+
+#for x in findPermutation(inputstr):
+#    print(x)
+print(len(findPermutation(inputstr)))
